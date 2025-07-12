f593f52f0af6536387a1ac87d067afc798f7247a
Tresorshingiro <112695595+Tresorshingiro@users.noreply.github.com>
2025-07-13 00:34:23 +0200

    created email booking notification

--- backend/controllers/stripeWebhooks.js
+++ backend/controllers/stripeWebhooks.js
@@ -1,5 +1,6 @@
 const {stripe} = require('stripe');
 const Booking = require('../models/Booking');
+const { inngest } = require('../inngest');
 
 const stripewebhooks = async (request, response) => {
     const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY);
@@ -14,19 +15,20 @@ const stripewebhooks = async (request, response) => {
 
     try{
         switch (event.type) {
-            case "payment_intent.succeeded": {
-                const paymentIntent = event.data.object;
-                const sessionList = await stripeInstance.checkout.sessions.list({
-                    payment_intent: paymentIntent.id
-                })
-
-                const session = sessionList.data[0]
-                const { bookingId } = session.metadata
+            case "checkout.session.completed": {
+                const session = event.data.object;
+                const { bookingId } = session.metadata;
 
                 await Booking.findByIdAndUpdate(bookingId, {
                     isPaid: true,
                     paymentLink: ""
-                })
+                });
+
+                //send confirmation email
+                await inngest.send({
+                    name: "app/show.booked",
+                    data: { bookingId }
+                });
 
                 break;
             }
--- backend/inngest/index.js
+++ backend/inngest/index.js
@@ -2,6 +2,7 @@ const { Inngest } = require('inngest');
 const User = require('../models/User');
 const Booking = require('../models/Booking');
 const Show = require('../models/Show');
+const { sendEmail } = require('../configs/nodeMailer');
 
 const inngest = new Inngest({ id: 'movie-ticket-booking' });
 
@@ -73,11 +74,40 @@ const releaseSeatsAndCancelBooking = inngest.createFunction(
     }
 )
 
+// Inngest function to send email when user books a show
+const sendBookingConfirmationEmail = inngest.createFunction(
+    {
+        id: 'send-booking-confirmation-email'
+    },
+    {event: 'app/show.booked'},
+    async ({event, step})=> {
+        const { bookingId } = event.data;
+
+        const booking = await Booking.findById(bookingId).populate({path: 'show', populate: {path: 'movie', model: 'Movie'}}).populate('user');
+
+        await sendEmail({
+            to: booking.user.email,
+            subject: `Booking Confirmation: "${booking.show.movie.title}" booked!`,
+            body:`<div style="font-family: Arial, sans-serif; line-height: 1.5;">
+                    <h2>Hi ${booking.user.name},</h2>
+                    <p>Your booking for <strong style="color: #F84565;">"${booking.show.movie.title}"</strong> is confirmed. </p>
+                    <p>
+                      <strong>Date:</strong> ${new Date(booking.show.showDateTime).toLocaleDateString('en-us', { timeZone: 'Africa/Kigali' })} <br/>
+                      <strong>Time:</strong> ${new Date(booking.show.showshowDateTime).toLocaleDateString('en-us', { timeZone: 'Africa/Kigali' })}
+                    </p>
+                    <p>Enjoy the show! 🍿</p>
+                    <p>Thanks for booking with us!<br/>- CinemaShow Team</p>
+                  </div>`
+        })
+    }
+)
+
 const functions = [
     syncUserCreation,
     syncUserDeletion,
     syncUserUpdation,
-    releaseSeatsAndCancelBooking
+    releaseSeatsAndCancelBooking,
+    sendBookingConfirmationEmail
 ];
 
 module.exports = { inngest, functions };
